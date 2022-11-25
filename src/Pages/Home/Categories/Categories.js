3de0b436de735e5058bd4632e5e24878e8fc16c2
S. M. Sujan <smsujon50@gmail.com>
2022-11-25 14:29:22 +0600

    Footer Design Complete

--- src/Pages/Home/Categories/Categories.js
+++ src/Pages/Home/Categories/Categories.js
@@ -23,7 +23,7 @@ const Categories = () => {
             <div className='grid grid-cols-2 lg:grid-cols-4 gap-5 transition-all'>
             {
                 categories.map(category => <Category 
-                    keys = {category._id} category={category}
+                    key = {category._id} category={category}
                 ></Category>)
             }
             </div>
