b7d1fccc9dc25eda36541e4629fd9c75859434d2
S. M. Sujan <smsujon50@gmail.com>
2022-11-25 16:29:27 +0600

    Log In page Design

--- src/Pages/User/Login/Login.js
+++ src/Pages/User/Login/Login.js
@@ -1,9 +1,46 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+import logImg from '../../../assets/user/login.webp';
 
 const Login = () => {
+    const {register, handleSubmit, formState: { errors }} = useForm();
+
+    const handleLogin = data => {
+        console.log(data);
+    }
     return (
-        <div>
-            LogIn
+        <div className='py-10 w-11/12 mx-auto'>
+            <div className='grid grid-cols-1 md:grid-cols-2'>
+                <div>
+                    <img className='w-full hidden md:block' src={logImg} alt="" />
+                </div>
+                <div className='flex justify-center'>
+                    <div className='md:w-1/2'>
+                        <h2 className='text-2xl md:text-4xl mb-2 text-center font-semibold'>Log In</h2>
+                        <hr className='md:hidden' />
+                        <form onSubmit={handleSubmit(handleLogin)}>
+                            <div className="form-control w-full">
+                                <label className="label"> <span className="label-text">Email</span></label>
+                                <input type="email" {...register("email", { required: "Type a valid Email" })} className="input input-bordered w-full" />
+                                {errors.email && <p className='text-primary'>{errors.email.message}</p>}
+                            </div>
+                            <div className="form-control w-full">
+                                <label className="label"> <span className="label-text">Password</span></label>
+                                <input type="password" {...register("password", {
+                                    required: "Password is required",
+                                    minLength: { value: 6, message: "Password must be 6 characters long" }
+                                })} className="input input-bordered w-full" />
+                                {errors.password && <p className='text-primary'>{errors.password.message}</p>}
+                            </div>
+                            <input className='btn btn-primary mt-4 w-full' value="Login" type="submit" />
+                        </form>
+                        <p className='mt-5'>New to CarBazar? <Link className='text-secondary' to="/signup">Sign Up</Link></p>
+                        <div className="divider">OR</div>
+                        <button className='btn w-full'>CONTINUE WITH GOOGLE</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/User/Signup/Signup.js
+++ src/Pages/User/Signup/Signup.js
@@ -10,18 +10,35 @@ const Signup = () => {
 
     const handleSignUp = (data) => {
         console.log(data)
-
+        {/*createUserWithEmail(data.email, data.password)
+            .then(res => {
+                const user = res.user;
+                console.log(user);
+                toast.success('Sign Up Successful.')
+                const userInfo = {
+                    displayName: data.name
+                }
+                updateInfo(userInfo)
+                    .then(() => {
+                        // saveUser(data.name, data.email);
+                    })
+                    .catch(err => console.log(err));
+            })
+            .catch(error => {
+                console.log(error)
+                setSignUPError(error.message)
+            });*/}
     }
     return (
         <div className='py-10 w-11/12 mx-auto'>
             <div className='grid grid-cols-1 md:grid-cols-2'>
                 <div>
-                    <h2 className='text-2xl md:text-4xl mb-2 text-center font-semibold'>Create an Account</h2>
                     <img className='w-full hidden md:block' src={signImg} alt="" />
-                    <hr className='md:hidden' />
                 </div>
                 <div className='flex justify-center'>
                     <div className='md:w-1/2'>
+                        <h2 className='text-2xl md:text-4xl mb-2 text-center font-semibold'>Sign Up</h2>
+                        <hr className='md:hidden'/>
                         <form onSubmit={handleSubmit(handleSignUp)}>
                             <div className="form-control w-full">
                                 <label className="label"> <span className="label-text">Name</span></label>
@@ -51,8 +68,6 @@ const Signup = () => {
                             <input className='btn btn-primary mt-4 w-full' value="Create Account" type="submit" />
                         </form>
                         <p className='mt-5'>Already have an account? <Link className='text-secondary' to="/login">Login</Link></p>
-                        <div className="divider">OR</div>
-                        <button className='btn w-full'>CONTINUE WITH GOOGLE</button>
                     </div>
                 </div>
             </div>
